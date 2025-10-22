688e5b5a10ac270771063401f068c65dc22a4896
shox089 <95170675+shox089@users.noreply.github.com>
2025-10-22 21:51:05 +0500

    Add configuration for bot token and API keys

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Render environment variables
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")  # OpenWeather API kaliti
+ADMIN_ID = os.getenv("6688725338")  # Admin ID
