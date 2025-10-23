7d1c7442f4fa399f36fb1dd3efe52efdfb89662c
shox089 <95170675+shox089@users.noreply.github.com>
2025-10-23 14:07:50 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")  # Render environment variables
+ADMIN_ID = os.getenv("ADMIN_ID")    # Admin ID
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")  # OpenWeather API kaliti
-ADMIN_ID = os.getenv("6688725338")  # Admin ID
+
+# Instagram login (agar kerak bo‘lsa)
+INSTAGRAM_USERNAME = os.getenv("INSTAGRAM_USERNAME")
+INSTAGRAM_PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
